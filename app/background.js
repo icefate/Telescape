e7c12fb74fc914957e50f677dc727de9e1aa8a97
icefate <zdklight@163.com>
2015-04-23 17:00:43 +0800

    delete

--- app/background.js
+++
@@ -1,9 +0,0 @@
-
-chrome.runtime.onMessage.addListener(  
-	function(request, sender, sendResponse) {  
-		//Send a single message to a native application.
- 		chrome.runtime.sendNativeMessage(request.HostName, {"Editor":request.Editor,"FilePath":request.FilePath}, function(response){
-			
-		});
- 	return true;  
-});  
