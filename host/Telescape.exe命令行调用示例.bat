462582b2e5dd1361cb2af1082852a73177bc3873
icefate <i@buckethead.cn>
2015-04-23 21:41:09 +0800

    init

---
+++ host/Telescape.exe命令行调用示例.bat
@@ -0,0 +1,3 @@
+@echo off
+"%~dp0Telescape.exe" "{\"Editor\":\"sublime\",\"FilePath\":\"E:\\APMServ5.2.6\\www\\htdocs\\t.php\"}"
+pause
---
+++ host/get.bat
@@ -0,0 +1,3 @@
+@echo off
+copy /Y "E:\CSharp Solution\Projects\Telescape\Telescape\bin\Debug\Telescape.exe" %~dp0Telescape.exe
+pause
